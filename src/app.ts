e8e35d7540a6c6abc5f902fb74bfff918576d82c
pyoja <113084653+pyoja@users.noreply.github.com>
2024-04-15 13:17:13 +0900

    메인화면 연결

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 const port = 3000;
 
+// 정적 파일 경로 설정
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
 app.listen(port, () => {
